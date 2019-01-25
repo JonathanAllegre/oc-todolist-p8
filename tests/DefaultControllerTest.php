79c3cb40c19a99d7e817bec5bdd2061d467acba0
jhon <jhon@ubuntuServer.home>
2019-01-25 18:14:59 +0100

    test LoginOk

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -8,17 +8,66 @@
 
 namespace App\Tests;
 
+
+use \Symfony\Bundle\FrameworkBundle\Client as Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function setUp()
     {
-        $client = static::createClient();
+        $this->client = static::createClient();
+    }
 
-        $crawler = $client->request('GET', '/');
+    /**
+     * USER NOT LOGIN ASSERT 302
+     */
+    public function testIndexNoLogin()
+    {
+        $client = $this->client->request('GET', '/');
+        $this->client->followRedirect();
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
 
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    /**
+     * USER LOGIN ASSERT 200
+     */
+    public function testIndexWithLogin()
+    {
+        $this->logIn($this->client);
+
+        $crawler = $this->client->request('GET', '/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Bienvenue sur Todo List")')->count()
+        );
+    }
+
+    protected function logIn(Client $client)
+    {
+        $session = $client->getContainer()->get('session');
+
+        $firewallName = 'main';
+        // if you don't define multiple connected firewalls, the context defaults to the firewall name
+        // See https://symfony.com/doc/current/reference/configuration/security.html#firewall-context
+        $firewallContext = 'main';
+
+        // you may need to use a different token class depending on your application.
+        // for example, when using Guard authentication you must instantiate PostAuthenticationGuardToken
+        $token = new UsernamePasswordToken('jonathan', null, $firewallName, array('ROLE_USER'));
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
     }
 }
