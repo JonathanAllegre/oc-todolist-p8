646bd795721496075f5ab8b78b14cbe0389f576c
jhon <jhon@ubuntuServer.home>
2019-02-11 18:54:53 +0100

    Create TaskServiceTest

---
+++ tests/Services/TaskServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Services;
+
+use PHPUnit\Framework\TestCase;
+
+class TaskServiceTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
