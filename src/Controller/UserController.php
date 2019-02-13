e458c9f73cf92277374b9e6994806df59aa5cf3b
jhon <jhon@ubuntuServer.home>
2019-02-13 19:35:45 +0100

    Add test for UserService

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,7 +8,6 @@ use App\Services\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
---
+++ tests/Services/UserServiceTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonathan
+ * Date: 2019-02-13
+ * Time: 19:03
+ */
+
+namespace App\Tests\Services;
+
+use App\Entity\User;
+use App\Services\UserService;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserServiceTest extends KernelTestCase
+{
+
+    public function testCreate()
+    {
+        // MOCK OBJECT MANAGER
+        $manager = $this->createMock(ObjectManager::class);
+        $manager
+            ->expects($this->any())
+            ->method('persist')
+            ->willReturn(true);
+        $manager
+            ->method('flush')
+            ->willReturn(true);
+
+        $mock['objectManager'] = $manager;
+
+        $userService = $this->initService($mock);
+
+        $result = $userService->create((new User())->setPassword('test'));
+
+        $this->assertNotEquals('test', $result->getPassword());
+    }
+
+    public function testEdit()
+    {
+        // MOCK OBJECT MANAGER
+        $manager = $this->createMock(ObjectManager::class);
+        $manager
+            ->expects($this->any())
+            ->method('persist')
+            ->willReturn(true);
+
+        $mock['objectManager'] = $manager;
+
+        $userService = $this->initService($mock);
+
+        $result = $userService->edit((new User())->setPassword('test'));
+
+        $this->assertNotEquals('test', $result->getPassword());
+    }
+
+    private function getContainer()
+    {
+        self::bootKernel();
+        // returns the real and unchanged service container
+        $container = self::$kernel->getContainer();
+        // gets the special container that allows fetching private services
+        $container = self::$container;
+
+        return $container;
+    }
+
+    public function initService(array $mock = null): UserService
+    {
+        $manager = $this->getContainer()->get(ObjectManager::class);
+        $encoder = $this->getContainer()->get(UserPasswordEncoderInterface::class);
+
+        if (isset($mock['objectManager'])) {
+            $manager = $mock['objectManager'];
+        }
+
+        if (isset($mock['encoder'])) {
+            $encoder = $mock['encoder'];
+        }
+
+        return new UserService($manager, $encoder);
+    }
+
+}
